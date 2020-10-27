0c09d1caac011dfdc1f4f0972c575c081a5e8b2b
Yiheng Shu <yhshu@smail.nju.edu.cn>
2020-10-27 21:48:53 +0800

    init

--- homework3/stock_prediction/main.py
+++ homework3/stock_prediction/main.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 
 class Neuron(nn.Module):
@@ -13,6 +14,42 @@ class Neuron(nn.Module):
 
 if __name__ == '__main__':
     neuron = Neuron()
-    input = torch.tensor([[1., -1.], 
-                          [1., -1.]])
+    inputs = torch.tensor([[55.22, 56.34, 55.52, 55.53],
+                           [56.34, 55.52, 55.53, 56.94],
+                           [55.52, 55.53, 56.94, 58.88],
+                           [55.53, 56.94, 58.88, 58.18],
+                           [56.94, 58.88, 58.18, 57.09],
+                           [58.88, 58.18, 57.09, 58.38],
+                           [58.18, 57.09, 58.38, 58.54],
+                           [57.09, 58.38, 58.54, 57.72],
+                           [58.38, 58.54, 57.72, 58.02],
+                           [58.54, 57.72, 58.02, 57.81],
+                           [57.72, 58.02, 57.81, 58.71],
+                           [58.02, 57.81, 58.71, 60.84],
+                           [57.81, 58.71, 60.84, 61.08],
+                           [58.71, 60.84, 61.08, 61.74],
+                           [60.84, 61.08, 61.74, 62.16],
+                           [61.08, 61.74, 62.16, 60.80]])
 
+    golden = torch.tensor(
+        [56.94, 58.88, 58.18, 57.09, 58.38, 58.54, 57.72, 58.02, 57.81, 58.71, 60.84, 61.08, 61.74, 62.16, 60.80,
+         60.87])
+
+    criterion = nn.MSELoss()
+    neuron = Neuron()
+    optimizer = optim.AdamW(neuron.parameters(), lr=0.0001)
+
+    print("[INFO] Start Training")
+    running_loss = 0.0
+    for epoch_idx in range(5):
+        optimizer.zero_grad()
+        outputs = neuron(inputs)
+        loss = criterion(outputs, golden)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print("[INFO] Finished Training")
+
+    with torch.no_grad():
+        outputs = neuron(inputs)
+        print(outputs)
